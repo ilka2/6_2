3df5f758e19d84acb9c444ca1366c7e0f0aeb978
ilka2 <kravtsov-ilya@mail.ru>
2024-10-11 06:15:59 +0300

    Добавьте файлы проекта.

---
+++ 6_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <list>
+#include <set>
+
+using namespace std;
+
+template<class T> void Print(T container)
+{
+    auto i = container.begin();
+
+    while (i != container.end())
+    {
+        cout << *i << " ";
+        i++;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    set<string> test_set = { "one", "two", "three", "four" };
+    list<string> test_list = { "one", "two", "three", "four" };
+    vector<string> test_vector = { "one", "two", "three", "four" };
+
+    Print(test_set);
+    Print(test_list);
+    Print(test_vector);
+
+    return 0;
+}
